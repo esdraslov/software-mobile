f7d247538cd9bcf3b21252f1e648fef7a999bdd2
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-04-10 15:46:01 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-	
-})
+namespace SOmobile {
+    let sfw
+    export enum OSV {
+        //% block='android 5'
+        AndroidFive,
+        //% block='android 6'
+        AndroidSix,
+        //% block='android 10'
+        AndroidTen,
+        //% block='android 9'
+        AndroidNine,
+        //% block='android 7'
+        AndroidSeven,
+        //% block='android 8'
+        AndroidEight,
+        //% block='iphone'
+        Iphone
+    }
+    export function exportSO(so: OSV){
+        sfw = so
+    }
+}
