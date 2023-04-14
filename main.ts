bd365b51eedf8b3a1219c7be99ed2147c214f92c
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-04-14 18:21:01 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -63,7 +63,7 @@ namespace SOmobile {
         radio.setGroup(force)
     }
     //% block='get app number %position% (start in 0)'
-    //% position.min=1 position.max=10000 position.default=1
+    //% position.min=1 position.max=10000 position.value=1
     export function getApp(position: number){
         return apps[position-1]
     }
